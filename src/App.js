c94a46b0472e489334189e84372095c693d0c303
unknown <zignale92@gmail.com>
2025-01-15 11:40:34 +0100

    converted complete application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function App() {
+  const [amount, SetAmount] = useState(1);
+  const [FromCur, SetFromCur] = useState('EUR');
+  const [ToCur, SetToCur] = useState('USD');
+  const [converted, SetConverted] = useState("");
+  const [IsLoading, SetIsLoading] = useState(false);
+
+  useEffect(function(){
+
+    async function convert() {
+      SetIsLoading(true);
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${FromCur}&to=${ToCur}`);
+    
+        const data = await res.json();
+        SetConverted(data.rates[ToCur]);
+        SetIsLoading(false);
+    }
+
+    if (FromCur === ToCur) return SetConverted(amount);
+    
+
+    convert();
+  }, [amount, FromCur, ToCur]);
+
   return (
     <div>
      <h1>Converter Money</h1>
-      <input type="text" />
-      <select>
+      
+      <input type="text" 
+        value={amount} onChange={(e) => SetAmount(Number(e.target.value))} 
+        disabled={IsLoading} />
+     
+      <select value={FromCur} onChange={(e) => SetFromCur(e.target.value)} disabled={IsLoading} >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select value={ToCur} onChange={(e) => SetToCur(e.target.value)} disabled={IsLoading}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{converted} {ToCur}</p>
     </div>
   );
 }
